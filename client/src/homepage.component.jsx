9fbef9d8a55f0dd87de8384414e98212b019276c
Sam <samuel1008@gmail.com>
2020-07-01 12:38:58 -0700

    fixed git untracked error

---
+++ client/src/homepage.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import './hompage.styles.css';
+
+const HomePage = () => (
+
+  <div className='homepage'>
+    <div className='directory-menu'>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>First Data Nashville</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>First Data Global</h1>
+          <span className='subtitle'>FShop Now</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>AmexDirect</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>hase Paymentech</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>CTV</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+
+
+
+
+)
+
+export default HomePage
